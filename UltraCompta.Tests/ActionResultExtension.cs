d113efd633a40423c9b28a948b900502ef934b42
Thomas Thiry <thomas.thiry@gmail.com>
2020-09-05 14:38:47 +0200

    Added .Response() extension method for tests

---
+++ UltraCompta.Tests/ActionResultExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UltraCompta.Tests
+{
+    public static class ActionResultExtension
+    {
+        public static string Response(this IActionResult actionResult)
+        {
+            var contentResult = actionResult as ContentResult;
+            return contentResult?.Content;
+        }
+    }
+}
