a8e7508df9a7ab710d51869e24ac2269c995f99e
Thomas Thiry <thomas.thiry@gmail.com>
2020-09-10 11:06:07 +0200

    Used CustomerSourceMock in tests to avoid accessing DB

--- UltraCompta.Tests/InvoiceCreationTests.cs
+++ UltraCompta.Tests/InvoiceCreationTests.cs
@@ -9,7 +9,7 @@ namespace UltraCompta.Tests
         [Fact]
         public void Generate_invoice()
         {
-            var controller = new HomeController(new OrderSourceMock(), new CustomerSource());
+            var controller = new HomeController(new OrderSourceMock(), new CustomerSourceMock());
 
             var invoice = controller.GenerateInvoice("GDE948").Response();
 
@@ -19,7 +19,7 @@ namespace UltraCompta.Tests
         [Fact]
         public void Generate_invoice_with_two_items()
         {
-            var controller = new HomeController(new OrderSourceMock(), new CustomerSource());
+            var controller = new HomeController(new OrderSourceMock(), new CustomerSourceMock());
 
             var invoice = controller.GenerateInvoice("GDE949").Response();
 
@@ -29,13 +29,25 @@ namespace UltraCompta.Tests
         [Fact]
         public void Generate_invoice_with_a_Belgian_customer()
         {
-            var controller = new HomeController(new OrderSourceMock(), new CustomerSource());
+            var controller = new HomeController(new OrderSourceMock(), new CustomerSourceMock());
 
             var invoice = controller.GenerateInvoice("GDE950").Response();
 
             invoice.Should().Be("<html><style>table {border: 1px solid black;} tr:first-of-type {font-weight:bold;} td { padding: 5px;}</style><h1>Invoice GDE950</h1><p>Client name: Kwatta</p><p>Client id: C-9844</p><table><tr><td>Description</td><td>Size</td><td>Quantity</td><td>Unit price</td><td>VAT</td><td>Total price</td></tr><tr><td>Pate a tartiner</td><td>500 g</td><td>3</td><td>3,25 &euro;</td><td>6&percnt;</td><td>10,33 &euro;</td></tr></table></html>");
         }
 
+    }
+
+    public class CustomerSourceMock : ICustomerSource
+    {
+        public string GetCustomerCountry(string id)
+        {
+            if (id == "C-9844")
+            {
+                return "BE";
+            }
+            return "IT";
+        }
     }
 
     public class OrderSourceMock : IOrderSource
