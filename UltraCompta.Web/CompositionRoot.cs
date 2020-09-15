8317dff4f0eb055cca6adc2efa33e9cdf415b02d
Thomas Thiry <thomas.thiry@gmail.com>
2020-09-15 10:22:49 +0200

    Replaced IoC container with PureDI

---
+++ UltraCompta.Web/CompositionRoot.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using UltraCompta.Web.Controllers;
+
+namespace UltraCompta.Web
+{
+    public class CompositionRoot : IControllerActivator
+    {
+        public object Create(ControllerContext context)
+        {
+            if (context.ActionDescriptor.ControllerTypeInfo == typeof(HomeController))
+            {
+                return new HomeController(new OrderSource(), new CustomerSource(), new InvoiceStorage());
+            }
+ 
+            return null;
+        }
+
+        public void Release(ControllerContext context, object controller)
+        {
+            
+        }
+    }}
